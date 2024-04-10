7abd97065e73475ca23b5330e94ed3475e53050f
Ahmet Alp Balkan <ahmetalpbalkan@gmail.com>
2024-04-09 23:13:17 -0700

    more yaml utils + tests

--- yaml.go
+++ yaml.go
@@ -1,11 +1,21 @@
 package main
 
 import (
+	"bytes"
 	"errors"
 	"fmt"
 
 	"gopkg.in/yaml.v3"
-	"k8s.io/klog/v2"
+)
+
+var (
+	yamlNodeKind = map[yaml.Kind]string{
+		yaml.DocumentNode: "DocumentNode",
+		yaml.SequenceNode: "SequenceNode",
+		yaml.MappingNode:  "MappingNode",
+		yaml.ScalarNode:   "ScalarNode",
+		yaml.AliasNode:    "AliasNode",
+	}
 )
 
 // validateDocumentIsSingleKubernetesObject validates that the input document is
@@ -26,16 +36,16 @@ func validateDocumentIsSingleKubernetesObject(doc *yaml.Node) error {
 	}
 
 	// make sure the doc is not a metav1.List
-	if kind, ok := getValue(rootNode, "kind"); ok && kind == "List" {
+	if kind, ok := getMapValue(rootNode, "kind"); ok && kind == "List" {
 		return errors.New("input is a meta/v1.List object, only single objects are supported")
 	}
 
 	// Ensure input has `metadata.managedField`
-	metadata, ok := getValueNode(rootNode, "metadata")
+	metadata, ok := getMapValueNode(rootNode, "metadata")
 	if !ok {
 		return errors.New(".metadata not found in the object (is it a valid Kubernetes object?)")
 	}
-	_, ok = getValueNode(metadata, "managedFields")
+	_, ok = getMapValueNode(metadata, "managedFields")
 	if !ok {
 		return errors.New(".metadata.managedFields not found in the object, use `kubectl get --show-managed-fields -o=yaml` to get the resource")
 	}
@@ -43,8 +53,8 @@ func validateDocumentIsSingleKubernetesObject(doc *yaml.Node) error {
 }
 
 // stripManagedFields removes the `metadata.managedFields` field from the given
-// yaml document.
-func stripManagedFields(rootDoc *yaml.Node) {
+// yaml document. Returns true if the field was found and removed.
+func stripManagedFields(rootDoc *yaml.Node) bool {
 	var metadataNode *yaml.Node
 
 	for i, c := range rootDoc.Content {
@@ -54,15 +64,54 @@ func stripManagedFields(rootDoc *yaml.Node) {
 		}
 	}
 	if metadataNode == nil {
-		return
+		return false
 	}
 
 	for i, c := range metadataNode.Content {
 		if c.Value == "managedFields" {
 			// remove the key and the value adjacent to it
 			metadataNode.Content = append(metadataNode.Content[:i], metadataNode.Content[i+2:]...)
-			klog.V(3).Info("stripped managedFields from metadata")
-			return
+			return true
 		}
 	}
+	return false
+}
+
+// getMapValueNode returns the value node of a mapping entry in given node or returns
+// false if the key is not found.
+func getMapValueNode(mappingNode *yaml.Node, key string) (*yaml.Node, bool) {
+	for i, content := range mappingNode.Content {
+		if content.Value == key {
+			return mappingNode.Content[i+1], true
+		}
+	}
+	return nil, false
+}
+
+// getMapValue returns the string value of a mapping entry in given node or returns
+// false if the value is not a scalar node or the key is not found.
+func getMapValue(mappingNode *yaml.Node, key string) (string, bool) {
+	valNode, ok := getMapValueNode(mappingNode, key)
+	if !ok || valNode.Kind != yaml.ScalarNode {
+		return "", false
+	}
+	return valNode.Value, true
+}
+
+// mappingNodeAsMap converts a given yaml object (kind=MappingNode) into
+// an unstructured Go map
+func mappingNodeAsMap(node *yaml.Node) (map[string]any, error) {
+	if node.Kind != yaml.MappingNode {
+		return nil, fmt.Errorf("expected a mapping node, got %v", yamlNodeKind[node.Kind])
+	}
+	// easiest way to do this is to round-trip
+	var b bytes.Buffer
+	if err := yaml.NewEncoder(&b).Encode(node); err != nil {
+		return nil, fmt.Errorf("error encoding node: %w", err)
+	}
+	var out map[string]any
+	if err := yaml.NewDecoder(&b).Decode(&out); err != nil {
+		return nil, fmt.Errorf("error decoding node: %w", err)
+	}
+	return out, nil
 }
--- yaml_test.go
+++ yaml_test.go
@@ -4,17 +4,29 @@ import (
 	"bytes"
 	"testing"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"gopkg.in/yaml.v3"
 )
 
 func TestStripManagedFields(t *testing.T) {
-	r := yamlNode(t, []byte(exampleObj))
-	stripManagedFields(r.Content[0])
+	t.Run("success", func(t *testing.T) {
+		r := yamlDoc(t, []byte(exampleObj))
+		require.True(t, stripManagedFields(r.Content[0]))
 
-	var b bytes.Buffer
-	require.NoError(t, yaml.NewEncoder(&b).Encode(r.Content[0]))
-	require.NotContains(t, b.String(), "managedFields")
+		var b bytes.Buffer
+		require.NoError(t, yaml.NewEncoder(&b).Encode(r.Content[0]))
+		require.NotContains(t, b.String(), "managedFields")
+	})
+	t.Run("no metadata", func(t *testing.T) {
+		r := yamlDoc(t, []byte("apiVersion: v1"))
+		require.False(t, stripManagedFields(r.Content[0]))
+	})
+
+	t.Run("no managedFields", func(t *testing.T) {
+		r := yamlDoc(t, []byte(`metadata: {"a": "b"}`))
+		require.False(t, stripManagedFields(r.Content[0]))
+	})
 }
 
 func TestValidateDocumentIsSingleKubernetesObject(t *testing.T) {
@@ -32,31 +44,31 @@ func TestValidateDocumentIsSingleKubernetesObject(t *testing.T) {
 		},
 		{
 			name:     "root object is not mappingNode",
-			node:     yamlNode(t, []byte("[1,2,3]")),
+			node:     yamlDoc(t, []byte("[1,2,3]")),
 			errcheck: require.Error,
 			errMsg:   "invalid document structure",
 		},
 		{
 			name:     "List object",
-			node:     yamlNode(t, []byte("apiVersion: v1\nkind: List\nitems: []")),
+			node:     yamlDoc(t, []byte("apiVersion: v1\nkind: List\nitems: []")),
 			errcheck: require.Error,
 			errMsg:   "List object",
 		},
 		{
 			name:     "no metadata",
-			node:     yamlNode(t, []byte("apiVersion: v1")),
+			node:     yamlDoc(t, []byte("apiVersion: v1")),
 			errcheck: require.Error,
 			errMsg:   "metadata not found",
 		},
 		{
 			name:     "no managedFields",
-			node:     yamlNode(t, []byte("metadata: {}")),
+			node:     yamlDoc(t, []byte("metadata: {}")),
 			errcheck: require.Error,
 			errMsg:   "managedFields not found",
 		},
 		{
 			name:     "valid object",
-			node:     yamlNode(t, []byte(exampleObj)),
+			node:     yamlDoc(t, []byte(exampleObj)),
 			errcheck: require.NoError,
 			errMsg:   "",
 		},
@@ -70,10 +82,66 @@ func TestValidateDocumentIsSingleKubernetesObject(t *testing.T) {
 			}
 		})
 	}
-
 }
 
-func yamlNode(t *testing.T, b []byte) *yaml.Node {
+func TestGetMapValueNode(t *testing.T) {
+	in := `k1: v1
+k2: {}`
+	doc := yamlDoc(t, []byte(in))
+	root := doc.Content[0]
+
+	k1, ok := getMapValueNode(root, "k1")
+	require.True(t, ok)
+	assert.Equal(t, "v1", k1.Value)
+
+	k2, ok := getMapValueNode(root, "k2")
+	require.True(t, ok)
+	assert.Equal(t, yaml.MappingNode, k2.Kind)
+
+	_, ok = getMapValueNode(root, "k3")
+	require.False(t, ok)
+}
+
+func TestGetMapValue(t *testing.T) {
+	in := `k1: v1
+k2: [1,2,3]`
+	doc := yamlDoc(t, []byte(in))
+	root := doc.Content[0]
+
+	v1, ok := getMapValue(root, "k1")
+	require.True(t, ok)
+	assert.Equal(t, "v1", v1)
+
+	_, ok = getMapValue(root, "k2")
+	require.False(t, ok)
+
+	_, ok = getMapValue(root, "k3")
+	require.False(t, ok)
+}
+
+func TestMappingNodeAsMap(t *testing.T) {
+	t.Run("not a mapping node", func(t *testing.T) {
+		n := yamlDoc(t, []byte(`[1,2,3]`))
+		_, err := mappingNodeAsMap(n.Content[0])
+		require.Error(t, err)
+	})
+	t.Run("valid map", func(t *testing.T) {
+		n := yamlDoc(t, []byte(`{"a": {"arr": [1,2,3]}, "b": "valB", "c": null, "d": true}`))
+		got, err := mappingNodeAsMap(n.Content[0])
+		require.NoError(t, err)
+		expected := map[string]any{
+			"a": map[string]any{
+				"arr": []any{1, 2, 3},
+			},
+			"b": "valB",
+			"c": nil,
+			"d": true,
+		}
+		assert.Equal(t, expected, got)
+	})
+}
+
+func yamlDoc(t *testing.T, b []byte) *yaml.Node {
 	t.Helper()
 	var doc yaml.Node
 	require.NoError(t, yaml.Unmarshal([]byte(b), &doc))
